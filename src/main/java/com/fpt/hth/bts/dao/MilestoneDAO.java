2e6bf038d67fa3907270c097be41543782f32560
Nguyen Ngoc Thanh Hai <hainnt1302@gmail.com>
2015-04-06 10:59:14 +0700

    add create milestone

---
+++ src/main/java/com/fpt/hth/bts/dao/MilestoneDAO.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.fpt.hth.bts.dao;
+
+import com.fpt.hth.bts.entity.Milestone;
+
+/**
+ *
+ * @author Administrator
+ */
+public class MilestoneDAO extends GenericDaoJpaImpl<Milestone, Integer>{
+    
+}
--- src/main/webapp/assets/js/script.js
+++ src/main/webapp/assets/js/script.js
@@ -6,14 +6,23 @@
 
 function toggleCreateLabel() {
     var obj = $("#create-label");
-    if(obj.hasClass("hidden")){
+    if (obj.hasClass("hidden")) {
         $("#create-label").removeClass("hidden");
-    }else{
+    } else {
         $("#create-label").addClass("hidden");
     }
     var color = generateLabel();
     var value = color.bg + "|" + color.text;
-    $("#demo-color").attr("style", "background-color: "+color.bg+";"+"color:"+color.text);
+    $("#demo-color").attr("style", "background-color: " + color.bg + ";" + "color:" + color.text);
     $("#label-color").val(value);
+}
+
+function toggleCreateMilestone() {
+    var obj = $("#create-milstone");
+    if (obj.hasClass("hidden")) {
+        $("#create-milstone").removeClass("hidden");
+    } else {
+        $("#create-milstone").addClass("hidden");
+    }   
 }
 
